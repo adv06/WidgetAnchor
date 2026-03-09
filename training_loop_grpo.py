fca90b5bfc49965e95a386c00af77f896ed6ce7e
adv06 <adveysnet@gmail.com>
2026-03-09 12:16:20 -0700

    add old policy to get ratio

--- training_loop_grpo.py
+++ training_loop_grpo.py
@@ -18,11 +18,13 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 
 frozen = copy.deepcopy(model) # for KL later
-
+old_model = copy.deepcopy(frozen) # store the old for policy updayes
 for param in frozen.parameters():
     param.requires_grad = False # no grad update 
 
-epochs = 100
+for param in old_model.parameters():
+    old_model.requires_grad = False 
+
 n = 5
 training_steps = 1000
 prompts = [] # training steps prompts
@@ -51,44 +53,55 @@ for i in range(training_steps):
         generations.append(tokens)
         completion_scores.append(compute_reward_code(text, ground_truths[i]))
         
-    completion_scores = torch.Tensor(completion_scores)
+    completion_scores = torch.tensor(completion_scores, device=model.device)
     completion_reg = (completion_scores-completion_scores.mean())/(torch.std(completion_scores) + 1e-8)
     prompt_len = inputs["input_ids"].shape[1] # number of tokens in the prompt
     
     losses = []
-    
     beta = 0.05
     for tokens, adv in zip(generations, completion_reg):
         tokens = tokens.unsqueeze(0) # expects a batch input
-        
+        adv = adv.detach() # should not carry gradients
         outputs = model(tokens)
         with torch.no_grad():
             outputs_ref = frozen(tokens)
+            outputs_old = old_model(tokens)
         
         logits = outputs.logits # batch, seq, tokens
         logits_ref = outputs_ref.logits
+        logits_old = outputs_old.logits
         
         logits = logits[:, :-1]
         logits_ref = logits_ref[:, :-1]
+        logits_old = logits_old[:, :-1]
         
         targets = tokens[:, 1:] # batch, token align the logits and targets, remember that logits has an extra dimension
         log_probs = F.log_softmax(logits, dim=-1)    
         log_probs_ref = F.log_softmax(logits_ref, dim=-1)
+        log_probs_old = F.log_softmax(logits_old, dim = -1)
         
         token_log_probs = log_probs.gather(-1, targets.unsqueeze(-1)).squeeze(-1)
         token_log_probs_ref = log_probs_ref.gather(-1, targets.unsqueeze(-1)).squeeze(-1)
+        token_log_probs_old = log_probs_old.gather(-1, targets.unsqueeze(-1)).squeeze(-1)
     
         completion_log_probs = token_log_probs[:, prompt_len-1: ] # get rid of prompt batch, tokens
         completion_log_probs_ref = token_log_probs_ref[:, prompt_len-1: ] 
+        completion_log_probs_old = token_log_probs_old[:, prompt_len-1:]
         
-        KL = (completion_log_probs - completion_log_probs_ref).mean() # KL divergence
-        logprob_sum = completion_log_probs.sum() #  sum
+        KL = (completion_log_probs_ref - completion_log_probs).mean() # KL divergence
+        logprob_sum = completion_log_probs.sum() #  sum --> changed to mean so that longer sequences dont dominate
+        logprob_old_sum = completion_log_probs_old.sum()
         
-        loss = -adv * logprob_sum + beta * KL
+        eps = 0.2
+        
+        ratio = torch.exp(logprob_sum - logprob_old_sum)
+        loss = -torch.min(adv * ratio,  adv * torch.clamp(ratio, 1 - eps, 1 + eps)) + beta * KL
         
         losses.append(loss)
     
     loss = torch.stack(losses).mean() # preserve computation graph
     optimizer.zero_grad()
     loss.backward()
+    torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
     optimizer.step()
+    old_model.load_state_dict(model.state_dict()) # snapshot AFTER update, as a copy not a reference
---
+++ training_loop_trl.py
@@ -0,0 +1,1 @@
+# implement in the future
